2b1a4092f9d60afb76edbd1177aa09e1a859d56a
Patryk Kudlik <patryk.kudlik@gmail.com>
2024-05-29 22:37:26 +0200

    feat: update Vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,30 +1,25 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig({
   resolve: {
     alias: [
       {
         find: '@',
-        replacement: path.resolve(__dirname, './')
+        replacement: resolve(__dirname, './')
       },
       {
         find: '~',
-        replacement: path.resolve(__dirname, './')
+        replacement: resolve(__dirname, './')
       }
     ]
   },
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: [
-        'index.html'
-      ],
-      output: {
-        entryFileNames: 'scripts/index.js',
-        chunkFileNames: 'scripts/index.[hash].js',
-        assetFileNames: 'styles/index.css'
-      }
+      input: {
+        index: resolve(__dirname, 'index.html'),
+      },
     }
   }
 })
